eee894ad32a04711e090f0bb6d08771f2eca9765
Vanquay <aabayo1@students.towson.edu>
2021-04-07 12:04:31 -0400

    Updated Models

--- backend/models/events.model.js
+++ backend/models/events.model.js
@@ -1,11 +1,15 @@
 const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
 
-const user=new Schema(
+const exerciseSchema=new Schema(
     {
         description:{type:String, required:true},
         address:{type:String,required:true},
         locationUpload:{data:Buffer,contentType:String, required:false},
+        peopleNeeded:{type:Number,required:true},
     },
     {timestamp:true}
 );
+
+const Exercise=mongoose.model('Exercise',exerciseSchema);
+module.exports=Exercise;
